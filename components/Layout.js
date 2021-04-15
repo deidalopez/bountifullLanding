eeb9b6d616ca4ea1b971279cb29c040cdef8179d
Deida <deidalopez1@gmail.com>
2021-04-14 21:47:41 -0700

    favicon

--- components/Layout.js
+++ components/Layout.js
@@ -12,6 +12,10 @@ const Layout = (props) => {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
         />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png" />
+        <meta name="msapplication-TileColor" content="#ffffff"/>
+        <meta name="msapplication-TileImage" content="/assets/favicon/ms-icon-144x144.png"/>
+        <meta name="theme-color" content="#ffffff"/>
       </Head>
       <div>
         {props.children}
